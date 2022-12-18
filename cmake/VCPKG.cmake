0b9e9324b4194e304edf9b77e315a8c65ad935a6
Xiangyu Kong <xykong421@outlook.com>
2022-12-18 09:04:16 +0800

    Update listfile and presentfile, and create VCPKG.cmake

---
+++ cmake/VCPKG.cmake
@@ -0,0 +1,62 @@
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    set(PROJ_OS_WINDOWS TRUE)
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+    set(PROJ_OS_LINUX TRUE)
+endif()
+
+set(TEMP_FILE "${CMAKE_BINARY_DIR}/tmp.txt")
+execute_process(COMMAND "nslookup" "github.com" OUTPUT_FILE ${TEMP_FILE} OUTPUT_QUIET)
+file(STRINGS ${TEMP_FILE} TEMP_FILE_STRING)
+string(REGEX MATCH "[0-9]\+\\.[0-9]\+\\.[0-9]\+\\.[0-9]\+" GITHUB_IP_ADDRESS ${TEMP_FILE_STRING})
+
+# message(STATUS ${GITHUB_IP_ADDRESS})
+execute_process(COMMAND "curl" "-G" "-s" "ipinfo.io/${GITHUB_IP_ADDRESS}/country" OUTPUT_FILE ${TEMP_FILE} OUTPUT_QUIET)
+file(STRINGS ${TEMP_FILE} GITHUB_IP_COUNTRY)
+
+# message(STATUS ${GITHUB_IP_COUNTRY})
+if(${GITHUB_IP_COUNTRY} STREQUAL "CN")
+    set(USE_PROXY TRUE)
+    set(PROXY_PREPEND "https://ghproxy.com/")
+endif()
+
+include(FetchContent)
+FetchContent_Declare(
+    vcpkg
+    GIT_REPOSITORY "${PROXY_PREPEND}https://github.com/microsoft/vcpkg.git"
+    GIT_TAG 2022.11.14
+)
+FetchContent_MakeAvailable(vcpkg)
+
+execute_process(COMMAND "git" "status" WORKING_DIRECTORY ${vcpkg_SOURCE_DIR} OUTPUT_FILE ${TEMP_FILE} OUTPUT_QUIET)
+file(STRINGS ${TEMP_FILE} TEMP_FILE_STRING)
+string(REGEX MATCH "nothing to commit, working tree clean" GIT_STATUS "${TEMP_FILE_STRING}")
+
+if("${GIT_STATUS}" STREQUAL "")
+    execute_process(COMMAND "git" "restore" "." WORKING_DIRECTORY ${vcpkg_SOURCE_DIR})
+endif()
+
+if(USE_PROXY)
+    if(PROJ_OS_WINDOWS)
+        set(VCPKG_BOOTSTRAP_SCRIPT "${vcpkg_SOURCE_DIR}/scripts/bootstrap.ps1")
+        file(READ ${VCPKG_BOOTSTRAP_SCRIPT} TEMP_FILE_STRING)
+        string(REGEX REPLACE "github\\.com \"" "ghproxy.com \"/github.com" TEMP_FILE_STRING ${TEMP_FILE_STRING})
+    elseif(PROJ_OS_LINUX)
+        set(VCPKG_BOOTSTRAP_SCRIPT "${vcpkg_SOURCE_DIR}/scripts/bootstrap.sh")
+        file(READ ${VCPKG_BOOTSTRAP_SCRIPT} TEMP_FILE_STRING)
+        string(REGEX REPLACE "github\\.com" "ghproxy.com/github.com" TEMP_FILE_STRING ${TEMP_FILE_STRING})
+    endif()
+    file(WRITE ${VCPKG_BOOTSTRAP_SCRIPT} ${TEMP_FILE_STRING})
+endif()
+
+if(EXISTS "${vcpkg_SOURCE_DIR}/vcpkg.exe" OR EXISTS "${vcpkg_SOURCE_DIR}/vcpkg")
+    message(STATUS "BUILDED!")
+else()
+    execute_process(COMMAND ".\\bootstrap-vcpkg.bat"
+        WORKING_DIRECTORY ${vcpkg_SOURCE_DIR}
+        RESULTS_VARIABLE RES
+        OUTPUT_QUIET
+    )
+endif()
+
+# message(STATUS "${vcpkg_SOURCE_DIR}")
+# message(STATUS "${RES}")
