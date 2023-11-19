b9c554247afb014ecff9e7f3924fa7824f62457f
XyKong <30373236+traversebitree@users.noreply.github.com>
2023-11-19 16:21:59 +0800

    Update VcpkgHandle.cmake

--- cmake/VcpkgHandle.cmake
+++ cmake/VcpkgHandle.cmake
@@ -16,10 +16,8 @@ set(_VCPKG_DOWNLOAD_DIR "${_VCPKG_ROOT_DIR}")
 set(_VCPKG_BINARY_DIR "${_VCPKG_ROOT_DIR}/vcpkg-build")
 
 if(WIN32)
-    set(_VCPKG_BOOTSTRAP_SCRIPT "${_VCPKG_SOURCE_DIR}/scripts/bootstrap.ps1")
     set(_VCPKG_EXECUTEABLE "${_VCPKG_SOURCE_DIR}/vcpkg.exe")
 elseif(UNIX)
-    set(_VCPKG_BOOTSTRAP_SCRIPT "${_VCPKG_SOURCE_DIR}/scripts/bootstrap.sh")
     set(_VCPKG_EXECUTEABLE "${_VCPKG_SOURCE_DIR}/vcpkg")
 endif()
 
@@ -37,41 +35,6 @@ if(NOT EXISTS "${_VCPKG_EXECUTEABLE}")
     FetchContent_MakeAvailable(_vcpkg)
 endif()
 
-execute_process(COMMAND "git" "status" WORKING_DIRECTORY "${_VCPKG_SOURCE_DIR}" OUTPUT_FILE "${__TEMP_FILE}" OUTPUT_QUIET)
-file(STRINGS "${__TEMP_FILE}" __TEMP_FILE_STRING)
-string(REGEX MATCH "nothing to commit, working tree clean" _GIT_STATUS "${__TEMP_FILE_STRING}")
-
-if("${_GIT_STATUS}" STREQUAL "")
-    execute_process(COMMAND "git" "restore" "." WORKING_DIRECTORY "${_VCPKG_SOURCE_DIR}")
-endif()
-
-if("${WIN32}" AND NOT EXISTS "${_VCPKG_EXECUTEABLE}")
-    execute_process(COMMAND ".\\bootstrap-vcpkg.bat"
-        WORKING_DIRECTORY "${_VCPKG_SOURCE_DIR}"
-        OUTPUT_QUIET
-    )
-elseif("${UNIX}" AND NOT EXISTS "${_VCPKG_EXECUTEABLE}")
-    execute_process(COMMAND "./bootstrap-vcpkg.sh"
-        WORKING_DIRECTORY "${_VCPKG_SOURCE_DIR}"
-        OUTPUT_QUIET
-    )
-endif()
-
-if(EXISTS "${_VCPKG_EXECUTEABLE}")
-    set(_VCPKG_EXECUTEABLE_FOUND TRUE)
-else()
-    set(_VCPKG_EXECUTEABLE_FOUND FALSE)
-endif()
-
-file(REMOVE "${__TEMP_FILE}")
-
-message(STATUS "VCPKG_EXECUTEABLE: ${_VCPKG_EXECUTEABLE}")
-
-if("${_VCPKG_EXECUTEABLE_FOUND}")
-    message(STATUS "VCPKG_EXECUTEABLE_FOUND: ${_VCPKG_EXECUTEABLE_FOUND}")
-else()
-    message(FATAL_ERROR "VCPKG_EXECUTEABLE_FOUND: ${_VCPKG_EXECUTEABLE_FOUND}")
-endif()
-
 set(VCPKG_INSTALLED_DIR "${_VCPKG_ROOT_DIR}/vcpkg-installed")
+set(Z_VCPKG_ROOT_DIR "${_VCPKG_SOURCE_DIR}")
 set(CMAKE_TOOLCHAIN_FILE "${_VCPKG_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
