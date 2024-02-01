e35e0928fb5a38af25b86336247daf622a5c7e0e
traversebitree <traversebitree@users.noreply.github.com>
2024-02-01 09:25:01 +0000

    Automated commit of cmake-clang-format changes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "fmt/core.h"
 
-int main(int argc,char const *argv[])
+int main(int argc, char const *argv[])
 {
     fmt::print("Hello! {}-{}-{}", "vcpkg", "handle", "cmake");
     return 0;
